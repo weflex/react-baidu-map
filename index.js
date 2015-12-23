ae3974460c581dfcd561f6f0d40c53435c732255
Yazhong Liu <yorkiefixer@gmail.com>
2015-12-23 22:43:23 +0800

    v1.2.2

--- index.js
+++ index.js
@@ -1,47 +1,84 @@
 "use strict";
-const React = require('react');
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var React = require('react');
 
 /**
  * @class BaiduMap
  */
-class BaiduMap extends React.Component {
+
+var BaiduMap = (function (_React$Component) {
+  _inherits(BaiduMap, _React$Component);
+
   /**
    * @constructor
    * @id {String} the id to create DOM id
    */
-  constructor(props) {
-    super(props);
-    this.id = props.id || 'allmap';
+
+  function BaiduMap(props) {
+    _classCallCheck(this, BaiduMap);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(BaiduMap).call(this, props));
+
+    _this.id = props.id || 'allmap';
+    return _this;
   }
   /**
    * @method componentDidMount
    */
-  componentDidMount() {
-    this._map = new BMap.Map(this.id);
-    this._map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
-    this._local = new BMap.LocalSearch(this._map, {
-      renderOptions: { map: this._map },
-      onInfoHtmlSet: poi => {
-        if (typeof this.props.onSelect === 'function') {
-          this.props.onSelect(poi.marker.getPosition());
+
+  _createClass(BaiduMap, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      this._map = new BMap.Map(this.id);
+      this._map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
+      this._local = new BMap.LocalSearch(this._map, {
+        renderOptions: { map: this._map },
+        onInfoHtmlSet: function onInfoHtmlSet(poi) {
+          if (typeof _this2.props.onSelect === 'function') {
+            _this2.props.onSelect(poi.marker.getPosition());
+          }
         }
-      }
-    });
-  }
-  /**
-   * @method render
-   */
-  render() {
-    return <div id={this.id} {...this.props}></div>;
-  }
-  /**
-   * @method search
-   * @param {String} text - the search keyword
-   */
-  search(text) {
-    this._local.search(text);
-  }
-}
+      });
+    }
+    /**
+     * @method render
+     */
+
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement('div', _extends({ id: this.id }, this.props));
+    }
+    /**
+     * @method search
+     * @param {String} text - the search keyword
+     */
+
+  }, {
+    key: 'search',
+    value: function search(text) {
+      this._local.search(text);
+    }
+  }]);
+
+  return BaiduMap;
+})(React.Component);
 
 BaiduMap.propTypes = {
   /**
@@ -54,4 +91,4 @@ BaiduMap.propTypes = {
   onSelect: React.PropTypes.func
 };
 
-export default BaiduMap;
+exports.default = BaiduMap;
---
+++ index.jsx
@@ -0,0 +1,57 @@
+"use strict";
+const React = require('react');
+
+/**
+ * @class BaiduMap
+ */
+class BaiduMap extends React.Component {
+  /**
+   * @constructor
+   * @id {String} the id to create DOM id
+   */
+  constructor(props) {
+    super(props);
+    this.id = props.id || 'allmap';
+  }
+  /**
+   * @method componentDidMount
+   */
+  componentDidMount() {
+    this._map = new BMap.Map(this.id);
+    this._map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
+    this._local = new BMap.LocalSearch(this._map, {
+      renderOptions: { map: this._map },
+      onInfoHtmlSet: poi => {
+        if (typeof this.props.onSelect === 'function') {
+          this.props.onSelect(poi.marker.getPosition());
+        }
+      }
+    });
+  }
+  /**
+   * @method render
+   */
+  render() {
+    return <div id={this.id} {...this.props}></div>;
+  }
+  /**
+   * @method search
+   * @param {String} text - the search keyword
+   */
+  search(text) {
+    this._local.search(text);
+  }
+}
+
+BaiduMap.propTypes = {
+  /**
+   * the id to create DOM id
+   */
+  id: React.PropTypes.string,
+  /**
+   * this function will be fired when user click a marker and the info bubble is shown
+   */
+  onSelect: React.PropTypes.func
+};
+
+export default BaiduMap;
