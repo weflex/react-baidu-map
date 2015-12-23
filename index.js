f31bf35cf26e4c1e8f63f1d357702a734d49a721
yorkie <yorkiefixer@gmail.com>
2015-12-23 18:08:13 +0800

    add comments

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 "use strict";
 import React from 'react';
 
+/**
+ * @class BaiduMap
+ */
 export class BaiduMap extends React.Component {
+  /**
+   * @constructor
+   * @id {String} the id to create DOM id
+   */
   constructor(props) {
     super(props);
     this.id = props.id || 'allmap';
   }
+  /**
+   * @method componentDidMount
+   */
   componentDidMount() {
     this._map = new BMap.Map(this.id);
     this._map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
@@ -18,10 +28,17 @@ export class BaiduMap extends React.Component {
       }
     });
   }
-  search(text) {
-    this._local.search(text);
-  }
+  /**
+   * @method render
+   */
   render() {
     return <div id={this.id} {...this.props}></div>;
+  }
+  /**
+   * @method search
+   * @param {String} text - the search keyword
+   */
+  search(text) {
+    this._local.search(text);
   }
 }
